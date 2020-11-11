3b4e80b1c3a28e8ea653c106b1aeebe85e584cf9
Diego San German <diegosange3@gmail.com>
2020-11-11 17:00:39 -0600

    Solicitudes HTTP y base de datos

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, FlatList, Button, StyleSheet, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, FlatList, Button, StyleSheet, Text, ActivityIndicator } from 'react-native';
 import Colors from '../../constants/Colors';
 import { useDispatch, useSelector } from 'react-redux';
 import CartItem from '../../components/shop/CartItem';
@@ -8,6 +8,8 @@ import * as ordersActions from '../../store/actions/orders';
 import Card from '../../components/UI/Card'
 
 const CartScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
+
     const cartTotalAmount = useSelector(state => state.cart.totalAmount);
     const cartItems = useSelector(state => {
         const transformedCartItems = [];
@@ -20,36 +22,42 @@ const CartScreen = props => {
                 sum: state.cart.items[key].sum
             });
         }
-        return transformedCartItems.sort((a, b) => a.productId > b.productId ? 1 : -1 );
+        return transformedCartItems.sort((a, b) => a.productId > b.productId ? 1 : -1);
     });
     const dispatch = useDispatch();
+
+    const sendOrderHandler = async () => {
+        setIsLoading(true);
+        await dispatch(ordersActions.addOrder(cartItems, cartTotalAmount))
+        setIsLoading(false);
+    }
 
     return <View style={styles.screen}>
         <Card style={styles.summary}>
             <Text style={styles.summaryText}>Total: <Text style={styles.amount}>${Math.round(cartTotalAmount.toFixed(2) * 100) / 100} </Text></Text>
-            <Button color={Colors.accent} 
-            title="Order Now" 
-            disabled={cartItems.length === 0} 
-            onPress={() => {
-                dispatch(ordersActions.addOrder(cartItems, cartTotalAmount))
-            }}
-            />
+            {isLoading ? (<ActivityIndicator size='small' color={Colors.primary} />) : (
+                <Button color={Colors.accent}
+                    title="Order Now"
+                    disabled={cartItems.length === 0}
+                    onPress={sendOrderHandler}
+                />
+            )}
         </Card>
         <FlatList
             data={cartItems}
             keyExtractor={item => item.productId}
             renderItem={itemData => (
-                <CartItem 
-                quantity={itemData.item.quantity} 
-                title={itemData.item.productTitle} 
-                amount={itemData.item.sum} 
-                deletable
-                onRemove={() => {
-                    dispatch(cartActions.removeFromCart(itemData.item.productId));
-                }} 
-            />
+                <CartItem
+                    quantity={itemData.item.quantity}
+                    title={itemData.item.productTitle}
+                    amount={itemData.item.sum}
+                    deletable
+                    onRemove={() => {
+                        dispatch(cartActions.removeFromCart(itemData.item.productId));
+                    }}
+                />
             )}
-            />
+        />
     </View>
 };
 
--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { FlatList } from 'react-native';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, FlatList, Platform, ActivityIndicator } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../../components/UI/HeaderButton';
 import OrderItem from '../../components/shop/OrderItem';
+import * as ordersAction from '../../store/actions/orders';
+import Colors from '../../constants/Colors';
 
 const OrdersScreen = props => {
+    const [isLoading, setIsLoading] = useState(false);
+
     const orders = useSelector(state => state.orders.orders);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        setIsLoading(true)
+        dispatch(ordersAction.fetchOrders()).then(() => {
+            setIsLoading(false);
+        });
+    }, [dispatch]);
+
+    if (isLoading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size='large' color={Colors.primary} />
+            </View>
+        )
+    }
+
     return <FlatList
         data={orders}
         renderItem={itemData => (
@@ -34,8 +55,14 @@ OrdersScreen.navigationOptions = navData => {
             </HeaderButtons>
         ),
     };
-
 };
 
+const styles= StyleSheet.create({
+    centered: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
 
 export default OrdersScreen;
